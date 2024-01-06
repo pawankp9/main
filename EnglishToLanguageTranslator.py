83eb746d1c02590eb737637a68ba3ce42a8ce80a
Pawan <pawan@Pawans-MacBook-Pro.local>
2024-01-07 02:28:00 +0530

    Bug fix for English table generation

--- EnglishToLanguageTranslator.py
+++ EnglishToLanguageTranslator.py
@@ -13,7 +13,7 @@ def languageTranslator(dataFrameEnglish, language, file_name_without_extension):
         llmResponse = chain.invoke({"dataTable": dataFrameEnglish, "language": language, "wordToExcelPrompt": wordToExcelPrompt})
 
         llmResponse = llmResponse.content
-        # st.write(f"\nSTEP1: LLMRESPONSE: {llmResponse}")
+        st.write(f"\nSTEP1: LLMRESPONSE: {llmResponse}")
 
         # data_start_index = llmResponse.find('[')  # Find the index where the JSON data starts
         # json_data_string = llmResponse[data_start_index:]  # Extract the JSON data string
@@ -21,11 +21,11 @@ def languageTranslator(dataFrameEnglish, language, file_name_without_extension):
 
         # Convert the JSON data string to a list of dictionaries
         llmResponseJSON = json.loads(llmResponse)
-        # st.write(f"\nSTEP3: llmResponseJSON: {llmResponseJSON}")
+        st.write(f"\nSTEP3: llmResponseJSON: {llmResponseJSON}")
 
         # Create DataFrame
         dataFrameTranslated = pd.DataFrame(llmResponseJSON)
-        # st.write(f"\nSTEP4: llmResponseJSON: {dataFrameTranslated}")
+        st.write(f"\nSTEP4: llmResponseJSON: {dataFrameTranslated}")
 
         # Save DataFrame to Excel
         # dataFrameTranslated.to_csv(file_name_without_extension + "_" + language + ".csv", index=False)
