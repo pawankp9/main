1eaf4110b4d752e804bab856aa3423a08c17477e
Pawan <pawan@Pawans-MacBook-Pro.local>
2024-01-06 23:47:25 +0530

    Added logs to translator function

--- EnglishToLanguageTranslator.py
+++ EnglishToLanguageTranslator.py
@@ -3,6 +3,7 @@ import json
 from prompt import *
 from llmClient import LLMClient
 import pandas as pd
+import streamlit as st
 
 def languageTranslator(dataFrameEnglish, language, file_name_without_extension):
 
@@ -12,18 +13,18 @@ def languageTranslator(dataFrameEnglish, language, file_name_without_extension):
         llmResponse = chain.invoke({"language": language, "dataTable": dataFrameEnglish})
 
         llmResponse = llmResponse.content
-        print(f"LLMRESPONSE: {llmResponse}")
+        st.write(f"LLMRESPONSE: {llmResponse}")
 
         data_start_index = llmResponse.find('[')  # Find the index where the JSON data starts
         json_data_string = llmResponse[data_start_index:]  # Extract the JSON data string
 
         # Convert the JSON data string to a list of dictionaries
         llmResponseJSON = json.loads(json_data_string)
-        print(f"LLMRESPONSEJSON: {llmResponseJSON}")
+        st.write(f"LLMRESPONSEJSON: {llmResponseJSON}")
 
         # Create DataFrame
         dataFrameTranslated = pd.DataFrame(llmResponseJSON)
-        print(f"DataFrame {dataFrameTranslated}")
+        st.write(f"DataFrame {dataFrameTranslated}")
 
         # Save DataFrame to Excel
         # dataFrameTranslated.to_csv(file_name_without_extension + "_" + language + ".csv", index=False)
